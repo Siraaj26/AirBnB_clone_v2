e52ae8b44a489220541ffba4b592fe4e0bf8d8e9
Siraaj Jeftha <63967985+Siraaj26@users.noreply.github.com>
2022-12-06 11:49:12 +0200

    sql script to create database environments for project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,12 @@
+-- this script prepares a MySQL server for the project
+-- creates project developement database with the name : hbnb_dev_db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- creating new user named : hbnb_dev with all privileges on the db hbnb_dev_db
+-- with the password : hbnb_dev_pwd if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+--
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+-- granting privileges to the new user
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- granting the SELECT privilege for the user hbnb_dev in the db performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
